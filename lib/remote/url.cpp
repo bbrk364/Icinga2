a6135e4c6d7ddc79c99f7eea937a22415822e70b
Jean Flach <jean-marcel.flach@netways.de>
2016-10-10 11:42:18 +0200

    Fix Url query without values
    fixes #12883

--- lib/remote/url.cpp
+++ lib/remote/url.cpp
@@ -36,7 +36,9 @@ Url::Url(const String& base_url)
 	if (url.GetLength() == 0)
 		BOOST_THROW_EXCEPTION(std::invalid_argument("Invalid URL Empty URL."));
 
-	size_t pHelper = url.Find(":");
+	size_t pHelper = String::NPos;
+	if (url[0] != '/')
+		pHelper = url.Find(":");
 
 	if (pHelper != String::NPos) {
 		if (!ParseScheme(url.SubStr(0, pHelper)))
@@ -257,17 +259,26 @@ String Url::Format(bool print_credentials) const
 			else
 				param += "&";
 
+			// Just one (or one empty) value
+			if (kv.second.size() == 1) {
+				param += key;
+				param += kv.second[0].IsEmpty() ?
+				    String() : "=" + Utility::EscapeString(kv.second[0], ACQUERY_ENCODE, false);
+				continue;
+			}
+
+			// Array
 			String temp;
 			for (const String s : kv.second) {
 				if (!temp.IsEmpty())
 					temp += "&";
 
 				temp += key;
-
 				if (kv.second.size() > 1)
 					temp += "[]";
 
-				temp += "=" + Utility::EscapeString(s, ACQUERY_ENCODE, false);
+				if (!s.IsEmpty())
+					temp += "=" + Utility::EscapeString(s, ACQUERY_ENCODE, false);
 			}
 			param += temp;
 		}
@@ -375,7 +386,11 @@ bool Url::ParseQuery(const String& query)
 		String key = token.SubStr(0, pHelper);
 		String value = Empty;
 
-		if (pHelper != token.GetLength() - 1)
+<<<<<<< HEAD
+		if (pHelper != String::NPos && pHelper != token.GetLength() - 1)
+=======
+		if (pHelper != token.NPos && pHelper != token.GetLength() - 1)
+>>>>>>> Fix Url query without values
 			value = token.SubStr(pHelper+1);
 
 		if (!ValidateToken(value, ACQUERY))
